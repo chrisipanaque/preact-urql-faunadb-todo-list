07ee21fb108c926123c591696b8e4867592ec980
Christian Ipanaque <christian.ipanaque@formidable.com>
2020-03-22 00:19:40 -0700

    add AddTodo component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { createClient, Provider } from "urql";
-import Todos from "./Todos";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 const client = createClient({
-	url: `${process.env.PREACT_APP_FAUNADB_URL}`,
+  url: `${process.env.PREACT_APP_FAUNADB_URL}`,
   fetchOptions: () => {
     const token = `${process.env.PREACT_APP_FAUNADB_TOKEN}`;
     return {
@@ -14,6 +15,7 @@ const client = createClient({
 export default () => {
   return (
     <Provider value={client}>
+      <AddTodo />
       <Todos />
     </Provider>
   );
