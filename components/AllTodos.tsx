9fbe0a8fd3afca98011b6edf128a5494961ad510
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-02-15 16:04:17 -0800

    fix any type in allTodos.map

--- components/AllTodos.tsx
+++ components/AllTodos.tsx
@@ -1,9 +1,9 @@
-import { AllTodosType } from '../interfaces';
+import { AllTodosType, TodoType } from '../interfaces';
 
 export default function AllTodos({ allTodos }: AllTodosType) {
   return (
     <>
-      {allTodos.map(({ _id, task }: any) => (
+      {allTodos.map(({ _id, task }: TodoType) => (
         <div key={_id}>{task}</div>
       ))}
     </>
